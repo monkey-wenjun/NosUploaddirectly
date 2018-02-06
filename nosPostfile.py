ceceee216ac84d90575702f161c6ea19c09a79aa
wenjun <hi@awen.me>
2018-02-06 14:40:51 +0800

    Update nosPostfile.py

--- nosPostfile.py
+++ nosPostfile.py
@@ -7,39 +7,41 @@ import hashlib
 import time
 import requests
 import json
+import datetime
 
 
-# 计算token
+# 计算签名
 
-def token(accesskey,secretkey):
-	
-	expirestime = int(time.time()+6000)
-	
-	json_data = {"Bucket":bucketname,"Object":objectname,"Expires":expirestime}
+		
+def sign(accesskey,secretkey,bucketname,objectname):
 
-	putpolicy = json.dumps(json_data)
-	
-	encodedputpolicy = base64.b64encode(putpolicy)
-	
-	signature = hmac.new(secretkey, encodedputpolicy, digestmod=hashlib.sha256).digest();
-	
-	encodedsign = base64.b64encode(signature)
-	
-	token = "UPLOAD " + accesskey + ":" + encodedsign + ":" + encodedputpolicy
-	
-	return token
+    expirestime = int(time.time()+6000)
+
+    json_data = {"Bucket":bucketname,"Object":objectname,"Expires":expirestime}
+
+    putpolicy = json.dumps(json_data)
+
+    encodedputpolicy = base64.b64encode(putpolicy)
+
+    signature = hmac.new(secretkey, encodedputpolicy, digestmod=hashlib.sha256).digest();
+
+    encodedsign = base64.b64encode(signature)
+
+    token = "UPLOAD " + accesskey + ":" + encodedsign + ":" + encodedputpolicy
+
+    return token
 
 # 获取最佳节点
 
 def getnode(bucketname):
 	
-	url ='http://lbs-eastchina1.126.net/lbs?version=1.0'+'&bucketname='+bucketname
-	
-	node = requests.get(url)
-	
-	nodeValue= node.json()
-	
-	return nodeValue['upload'][0]
+    url ='http://lbs-eastchina1.126.net/lbs?version=1.0'+'&bucketname='+bucketname
+
+    node = requests.get(url)
+
+    nodeValue= node.json()
+
+    return nodeValue['upload'][0]
 
 # 分块上传
 
@@ -47,46 +49,55 @@ def postfile(xnostoken,bucketname,
 			objectname,offset,
 			complete,host,contentlength,
 			contenttype,filepath):
+    gmtformat = '%a, %d %b %Y %H:%M:%S GMT'
+    gmtdate = datetime.datetime.utcnow().strftime(gmtformat)
+    querystring = {"offset":offset,"complete":complete,"version":1.0}
 
-	querystring = {"offset":offset,"complete":complete,"version":1.0}
+    headers = {'Host':host,'Content-Length':contentlength,
+			'x-nos-token':xnostoken,'Content-type':contenttype,'Date':gmtdate}
 
-	headers = {'Host':host,'Content-Length':contentlength,
-				'x-nos-token':xnostoken,'Content-type':contenttype}
+    requestaddress = getnode(bucketname)
+
+    requestaddress = str(requestaddress)
+
+    url = requestaddress+'/'+bucketname+'/'+objectname
+
+    with open(filepath, 'r') as f:
+
+	    requestapi = requests.post(url,params=querystring,headers=headers,data=f)
 	
-	requestaddress = getnode(bucketname)
-	
-	requestaddress = str(requestaddress)
-	
-	url = requestaddress+'/'+bucketname+'/'+objectname
+	    print(requestapi.json())
 
-	with open(filepath, 'r') as f:
 
-		r = requests.post(url,params=querystring,headers=headers,data=f)
-		
-		print(r.json())
+# 各必传参数
+
+def main():
+
+    bucketname = 'netease01' 
+
+    objectname = '5.jpg' 
+
+    accesskey = "" 
+
+    secretkey = ""
+
+    offset = 0
+
+    complete = True
+
+    host = 'nos-eastchina1.126.net' 
+
+    contenttype = 'image/jpeg'
+
+    contentlength = '34771'
+
+    filepath = '/Users/wenjun/PycharmProjects/Test/test.jpg'
+
+    xnostoken = sign(accesskey,secretkey,bucketname,objectname)
+
+    postfile(xnostoken,bucketname,objectname,offset,complete,host,contentlength,contenttype,filepath)
+
 
 if __name__ == '__main__':
 	
-	bucketname = 'netease01'
-	
-	objectname = '12344.jpg'
-	
-	accesskey = ""
-	
-	secretkey = ""
-	
-	xnostoken = token(accesskey,secretkey)
-	
-	offset = 0
-	
-	complete = True
-	
-	host = 'nos-eastchina1.126.net'
-	
-	contenttype = 'image/jpeg'
-	
-	contentlength = '200000'
-	
-	filepath = '/Users/wenjun/PycharmProjects/Test/test.jpg'
-	
-	postfile(xnostoken,bucketname,objectname,offset,complete,host,contentlength,contenttype,filepath)
+    main()
